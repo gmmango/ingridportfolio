c790c221622d442d4bc7e3e16ae7cdd1ddafeb9a
gmmango <gmmango95@gmail.com>
2025-10-23 16:48:57 +0100

    file changes

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,12 @@
 import type { NextConfig } from 'next';
 
-const repoName = 'ingridportfolio'; // Change to your GitHub repo name
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  // Skip ESLint errors during next build (CI)
-  eslint: { ignoreDuringBuilds: true }, // official option
-  // Skip TypeScript errors during next build (CI)
-  typescript: { ignoreBuildErrors: true }, // official option
-  basePath: isProd ? `/${repoName}` : '',
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  basePath: '',
+  assetPrefix: ''
 };
 
 export default nextConfig;
